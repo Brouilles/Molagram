344bc68a35ec951a2ebfce4288326ad67ad84032
DEZEIRAUD Gaëtan <gaetan@dezeiraud.com>
2021-10-29 11:06:09 +0200

    add logic

---
+++ DataModel.cs
@@ -0,0 +1,194 @@
+﻿using Molagram.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Molagram
+{
+    public struct Species
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public double MolarMass { get; set; }
+        public string URL { get; set; }
+    };
+
+    public class DataModel : ViewModelBase
+    {
+        private readonly string m_filePath = "ChemicalSpecies.ini";
+
+        // CurrentChemicalSpecies
+        private Species m_currentChemicalSpecies;
+        public Species CurrentChemicalSpecies
+        {
+            get => m_currentChemicalSpecies;
+            set
+            {
+                if (m_currentChemicalSpecies.Symbol != value.Symbol)
+                {
+                    m_currentChemicalSpecies = value;
+                    OnPropertyChanged(nameof(CurrentChemicalSpecies));
+                }
+            }
+        }
+
+        // ChemicalSpecies
+        private ObservableCollection<Species> m_chemicalSpecies = new ObservableCollection<Species>();
+        public ReadOnlyObservableCollection<Species> ChemicalSpecies
+        {
+            get;
+        }
+
+        // Units
+        private ObservableCollection<string> m_units = new ObservableCollection<string>();
+        public ReadOnlyObservableCollection<string> Units
+        {
+            get;
+        }
+        
+        // CurrentUnit
+        private string m_currentUnit;
+        public string CurrentUnit
+        {
+            get => m_currentUnit;
+            set
+            {
+                if (m_currentUnit != value)
+                {
+                    m_currentUnit = value;
+                    m_weight = m_currentChemicalSpecies.MolarMass * m_mole; // Gram
+                    m_weight = ConvertGramsToUnits(m_weight);
+                    OnPropertyChanged(nameof(CurrentUnit));
+                    OnPropertyChanged(nameof(Weight));
+                }
+            }
+        }
+
+        // Mole
+        private double m_mole;
+        public double Mole
+        {
+            get => m_mole;
+            set
+            {
+                if (m_mole != value)
+                {
+                    m_mole = value;
+                    m_weight = m_currentChemicalSpecies.MolarMass * m_mole; // Gram
+                    m_weight = ConvertGramsToUnits(m_weight);
+                    OnPropertyChanged(nameof(Mole));
+                    OnPropertyChanged(nameof(Weight));
+                }
+            }
+        }
+
+        // Weight
+        private double m_weight;
+        public double Weight
+        {
+            get => m_weight;
+            set
+            {
+                if (m_weight != value)
+                {
+                    m_weight = value;
+                    var tempWeight = ConvertToGrams(m_weight);
+                    m_mole = tempWeight / m_currentChemicalSpecies.MolarMass; // Gram
+                    OnPropertyChanged(nameof(Weight));
+                    OnPropertyChanged(nameof(Mole));
+                }
+            }
+        }
+
+        // Constructor
+        public DataModel()
+        {
+            ChemicalSpecies = new ReadOnlyObservableCollection<Species>(m_chemicalSpecies);
+            Units = new ReadOnlyObservableCollection<string>(m_units);
+            ReadChemicalSpecies();
+        }
+
+        private async void ReadChemicalSpecies()
+        {
+            m_mole = 0.0;
+            m_weight = 0.0;
+
+            // Units
+            m_units.Add("g");
+            m_units.Add("kg");
+            m_units.Add("oz");
+            m_units.Add("lb");
+
+            m_currentUnit = m_units[0];
+
+            // Load species from file
+            try
+            {
+                var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+                var file = await folder.GetFileAsync(m_filePath);
+                var readFile = await Windows.Storage.FileIO.ReadLinesAsync(file);
+                foreach (var line in readFile)
+                {
+                    string[] lineParams = line.Split(' ');
+                    m_chemicalSpecies.Add(new Species
+                    {
+                        Name = lineParams[0],
+                        Symbol = lineParams[1],
+                        MolarMass = double.Parse(lineParams[2]),
+                        URL = lineParams[3]
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                ContentDialog dialog = new ContentDialog()
+                {
+                    Title = "Error",
+                    Content = "Can't load the 'ChemicalSpecies.ini' file.",
+                    CloseButtonText = "Ok"
+                };
+                await dialog.ShowAsync();
+            }
+        }
+
+        // Methods
+        private double ConvertGramsToUnits(double gram)
+        {
+            switch (m_currentUnit)
+            {
+                case "kg":
+                    return gram / 1000;
+                case "oz":
+                    return gram / 28.3495231;
+                case "lb":
+                    return gram / 453.59237;
+                default: // g
+                    return gram;
+            }
+        }
+
+        private double ConvertToGrams(double value)
+        {
+            switch (m_currentUnit)
+            {
+                case "kg":
+                    return value * 1000;
+                case "oz":
+                    return value * 28.3495231;
+                case "lb":
+                    return value * 453.59237;
+                default: // g
+                    return value;
+            }
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,22 +1,97 @@
-﻿using Windows.Foundation;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.UI.Popups;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Molagram
 {
+    public class DecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString().Replace(".", ",");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString().Replace(".", ",");
+        }
+    }
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public DataModel ViewModel { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
 
             ApplicationView.PreferredLaunchViewSize = new Size(860, 350);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            this.ViewModel = new DataModel();
+        }
+
+        // Events
+        private async void AboutButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ContentDialog dialog = new ContentDialog()
+            {
+                Title = "About",
+                Content = "Molagram - Copyright © 2021 Dezeiraud Gaëtan (gaetan.dezeiraud.com), Chloé Vinour, Honorine Claudot. All rights reserved.",
+                CloseButtonText = "Ok"
+            };
+
+            await dialog.ShowAsync();
+        }
+
+        private void ChemicalSpecies_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            if (comboBox == null) return;
+
+            var content = comboBox.SelectedValue as Species?;
+            if (content != null)
+            {
+                this.ViewModel.CurrentChemicalSpecies = (Species)content;
+                this.ViewModel.Mole = 1;
+                this.ViewModel.Weight = this.ViewModel.CurrentChemicalSpecies.MolarMass;
+            }
+        }
+
+        private void Units_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            if (comboBox == null) return;
+
+            var content = comboBox.SelectedValue as string;
+            if (content != null)
+            {
+                this.ViewModel.CurrentUnit = content;
+            }
+        }
+
+        private void WikipediaButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (this.ViewModel.CurrentChemicalSpecies.URL != null)
+            {
+                string uriToLaunch = this.ViewModel.CurrentChemicalSpecies.URL;
+                var uri = new Uri(uriToLaunch);
+
+                _ = Windows.System.Launcher.LaunchUriAsync(uri);
+            }
         }
     }
 }
