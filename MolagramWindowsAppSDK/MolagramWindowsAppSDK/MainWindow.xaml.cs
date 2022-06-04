943ff7eb6e2bbe2a2d3dc908e7f4db03486a02d0
DEZEIRAUD Gaëtan <gaetan@dezeiraud.com>
2022-06-04 10:36:17 +0200

    Add mica effect on WindowsAppSDK version for 22h2
    Don't work on 22h1

--- MolagramWindowsAppSDK/MolagramWindowsAppSDK/MainWindow.xaml.cs
+++ MolagramWindowsAppSDK/MolagramWindowsAppSDK/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using WinUIEx;
-using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -9,14 +8,11 @@ namespace MolagramWindowsAppSDK
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : WindowEx
     {
         public MainWindow()
         {
             this.InitializeComponent();
-
-            Title = "Molagram";
-            this.SetWindowSize(860, 350);
         }
     }
 }
